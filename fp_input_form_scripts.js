7aa037f77805be339b545cca196fa254dd66b71b
jlwebpages <jerlaguna@gmail.com>
2016-11-24 21:54:37 -0800

    Test to see if we can determine if iOS is running.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -553,6 +553,7 @@ function build_regular_season_form()
    d.writeln('');
    d.writeln('   temp_window = window.open(mail_msg,"","top=0,left=0,width=100,height=100");');
    d.writeln('');
+   d.writeln('if(navigator.platform.indexOf("iOS")>-1)alert("iOS");');
    d.writeln('   setTimeout(function(){temp_window.close()},100);');
    d.writeln('');
    d.writeln('   return true;');
@@ -2373,7 +2374,7 @@ function build_post_season_form()
    d.writeln('   {');
    d.writeln('      mail_msg += escape("\\n\\nThese picks were generated from the NFL Odds.");');
    d.writeln('   }');
-   d.writeln('if(navigator.platform.indexOf("iOS")>-1)alert("iOS");');
+   d.writeln('alert(navigator.platform);');
    d.writeln('   temp_window = window.open(mail_msg,"","top=0,left=0,width=100,height=100");');
    d.writeln('');
    d.writeln('   setTimeout(function(){temp_window.close()},100);');
