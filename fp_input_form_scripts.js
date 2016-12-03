7e50cc252a9e93fc9477e61f8ded5c2b5fcd07ff
jlwebpages <jerlaguna@gmail.com>
2016-12-02 21:22:19 -0800

    Replace _blank with _top in window.open.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -551,7 +551,7 @@ function build_regular_season_form()
    d.writeln('');
    d.writeln('   if (navigator.platform.substring(0,2) == "iP")');
    d.writeln('   {');
-   d.writeln('      temp_window = window.open(mail_msg,"_blank","top=0,left=0,width=100,height=100");');
+   d.writeln('      temp_window = window.open(mail_msg,"_top","top=0,left=0,width=100,height=100");');
    d.writeln('');
    d.writeln('      setTimeout(function(){temp_window.close()},1000);');
    d.writeln('   }');
@@ -2378,7 +2378,7 @@ function build_post_season_form()
    d.writeln('');
    d.writeln('   if (navigator.platform.substring(0,2) == "iP")');
    d.writeln('   {');
-   d.writeln('      temp_window = window.open(mail_msg,"_blank","top=0,left=0,width=100,height=100");');
+   d.writeln('      temp_window = window.open(mail_msg,"_top","top=0,left=0,width=100,height=100");');
    d.writeln('');
    d.writeln('      setTimeout(function(){temp_window.close()},1000);');
    d.writeln('   }');
