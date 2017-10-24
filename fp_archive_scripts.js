2f7fc22dc7bca940939f098b401d27e02a0bd004
jlwebpages <jerlaguna@gmail.com>
2017-10-23 20:41:42 -0700

    2017 Week 7 Final update.

--- fp_archive_scripts.js
+++ fp_archive_scripts.js
@@ -121,7 +121,7 @@ function display_archive_page(archive_year)
    d.writeln('');
    d.writeln('      w.archive.location.href="fp_forms_'+archive_year+'.html";');
    d.writeln('');
-   d.writeln('      return;');
+   d.writeln('      return true;');
    d.writeln('   }');
    d.writeln('');
    d.writeln('');
@@ -217,15 +217,15 @@ function display_archive_page(archive_year)
    d.writeln('');
    d.writeln('      w.archive.location.href="fp_forms_'+archive_year+'.html";');
    d.writeln('');
-   d.writeln('      return;');
+   d.writeln('      return true;');
    d.writeln('   }');
    d.writeln('');
    d.writeln('');
    d.writeln('   function open_results_window()');
    d.writeln('   {');
-   d.writeln('      window.open("fp_results_'+archive_year+'.html","fp_picks","");');
-   d.writeln('      top.display_frame("fp_picks");');
-   d.writeln('      return;');
+   d.writeln('      window.open("fp_results_'+archive_year+'.html","fp_main","");');
+   d.writeln('      top.display_frame("fp_main");');
+   d.writeln('      return true;');
    d.writeln('   }');
    d.writeln('');
    d.writeln('');
