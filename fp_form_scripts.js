edc9bfe27252b5e05e11c2dc11c6556103146f79
jlwebpages <jerlaguna@gmail.com>
2017-11-10 23:40:15 -0800

    Undo test.

--- fp_form_scripts.js
+++ fp_form_scripts.js
@@ -5,12 +5,10 @@ function adjust_mobile_viewport_height(document)
    {
       if ( (window.screen.height > window.screen.width) && (top.document.getElementById("viewport") != null) )
       {
-         var document_height   = document.body.scrollHeight;
-         var viewport_scale    = (window.screen.height/document_height) * 0.9;
+         var document_height  = document.body.scrollHeight;
+         var viewport_scale   = window.screen.height/document_height;
+         var viewport_content = "height=" + document_height + "px, initial-scale=" + viewport_scale;
 
-         var viewport_content = "width=device-width, initial-scale=" + viewport_scale;
-         alert("DSH: " + document.body.scrollHeight + "  DOH: " + document.body.offsetHeight + "  DEOH: " + document.documentElement.offsetHeight);
-         alert(viewport_content);
          top.document.getElementById("viewport").setAttribute("content",viewport_content);
       }
    }
