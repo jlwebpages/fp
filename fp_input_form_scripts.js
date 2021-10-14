cf1dd94f986a2ab8892e19cdb35899a011d49c41
jlwebpages <jerlaguna@gmail.com>
2021-10-13 22:33:01 -0700

    Test.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -1336,7 +1336,9 @@ function build_regular_season_form()
    d.writeln('');
    d.writeln('   // Display loading indicator while call to XMLHttpRequest is working.');
    d.writeln('');
+   d.writeln('   if (suppress_alerts == false) alert("before loading indicator");'); //JLJL
    d.writeln('   top.display_frame("fp_loading_indicator",0);');
+   d.writeln('   if (suppress_alerts == false) alert("after loading indicator");'); //JLJL
    d.writeln('');
    d.writeln('   // Get the NFL Odds from the internet.');
    d.writeln('');
