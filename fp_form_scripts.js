33998a9355e623d1db3929d0511e3e04539d9e05
jlwebpages <jerlaguna@gmail.com>
2022-09-18 15:54:43 -0700

    Test.

--- fp_form_scripts.js
+++ fp_form_scripts.js
@@ -4520,8 +4520,10 @@ function build_regular_season_form()
 
    if ( (top.gv.mobile == true) && (navigator.platform != "iPad") )
    {
-      d.body.scrollLeft = 0;
-      d.body.scrollTop  = 0;
+      if (top.gv.rs_players[top.gv.player_index-1] == "JL") alert("JL");
+      window.scrollTo(0,0);
+      //d.body.scrollLeft = 0;
+      //d.body.scrollTop  = 0;
    }
    else
    {
