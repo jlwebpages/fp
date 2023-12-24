1f00599186aa5fe2f881a5cb910ca0875f8de615
jlwebpages <jerlaguna@gmail.com>
2023-12-24 11:42:08 -0800

    Minor cosmetic updates.

--- fp_form_scripts.js
+++ fp_form_scripts.js
@@ -1655,7 +1655,7 @@ function build_post_season_form()
             d.writeln('<td style="padding: 0px" class="gr1_border">');
          }
 
-         d.writeln('<input type=text'+input_tag_class+'style="font-size: 10pt" value='+visitor_scores[gi-1]+' size="'+input_field_size+'" maxlength="2" name="visitor'+gi+'_score">');
+         d.writeln('<input type=text'+input_tag_class+'style="border-radius: 0px; font-size: 10pt; width: 25px" value='+visitor_scores[gi-1]+' size="'+input_field_size+'" maxlength="2" name="visitor'+gi+'_score">');
          d.writeln('</td>');
       }
       else
@@ -1737,7 +1737,7 @@ function build_post_season_form()
          {
             d.writeln('<td style="padding: 0px" class="br2_border">');
          }
-         d.writeln('<input type=text'+input_tag_class+'style="font-size: 10pt" value='+home_scores[gi-1]+' size="'+input_field_size+'" maxlength="2" name="home'+gi+'_score">');
+         d.writeln('<input type=text'+input_tag_class+'style="border-radius: 0px; font-size: 10pt; width: 25px" value='+home_scores[gi-1]+' size="'+input_field_size+'" maxlength="2" name="home'+gi+'_score">');
          d.writeln('</td>');
       }
       else
@@ -2300,15 +2300,6 @@ function build_post_season_form()
 
    if (mode == "prelim")
    {
-      for (var gi = 1; gi <= number_of_games_to_display; gi++)
-      {
-         if ( (post_season_winners[gi-1] != "V") && (post_season_winners[gi-1] != "H") )
-         {
-            d.writeln('<script>window.top.gv.focus_element(document.fp_scores.visitor'+gi+'_score);</'+'script>');
-            break;
-         }
-      }
-
       if (window.top.gv.get_scores_timer != null)
       {
          clearInterval(window.top.gv.get_scores_timer);
@@ -4310,7 +4301,7 @@ function build_regular_season_form()
             {
                if (mn_pts_value == 0) mn_pts_value = "";
 
-               d.writeln('<input type=text class="default_text border_radius" style="border: 1px solid black" name="mn_points" size="3" maxlength="3" value="'+mn_pts_value+'"');
+               d.writeln('<input type=text class="default_text border_radius" style="border: 1px solid black; width: 35px" name="mn_points" size="3" maxlength="3" value="'+mn_pts_value+'"');
                d.writeln('              onChange="get_mn_points(document);return true;"');
                d.writeln('            onKeyPress="if (window.event.keyCode==13) {window.event.keyCode=0; get_mn_points(document); calculate_prelim_scores(document); return true;}">');
             }
@@ -4468,17 +4459,6 @@ function build_regular_season_form()
       if ( (tie_breaker_needed == true) && (unable_to_break_tie == false) )
       {
          window.top.gv.focus_element(d.fp_results.mn_points);
-      }
-      else
-      {
-         for (var i = 1; i <= number_of_rs_games; i++)
-         {
-            if (winners[i-1] == "0")
-            {
-               d.writeln('<script>top.gv.focus_element(document.fp_results.winner'+i+');</'+'script>');
-               break;
-            }
-         }
       }
 
       if (window.top.gv.get_scores_timer != null)
